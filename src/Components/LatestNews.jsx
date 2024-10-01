bd7776eca0a0a9cff3c0fff6028004b2a885af0b
Isabelle El Rafih <isorafi3@live.com>
2024-10-01 13:56:26 +0300

    used MUI components fro the latestNews page

--- src/Components/LatestNews.jsx
+++ src/Components/LatestNews.jsx
@@ -5,6 +5,15 @@ import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { getLatestNews } from "../api";
 import { setLatestNews, setStatus, setError } from "../reducers/newsReducer"; //used to update the Redux store
+import {
+  Grid2,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Button,
+  CircularProgress,
+} from "@mui/material";
 
 function LatestNews() {
   const dispatch = useDispatch();
@@ -53,7 +62,18 @@ function LatestNews() {
   };
 
   if (status === "loading") {
-    return <p>Loading latest news...</p>;
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <CircularProgress /> {/* Display loading spinner */}
+      </div>
+    );
   }
 
   if (error) {
@@ -62,32 +82,67 @@ function LatestNews() {
 
   return (
     <div className="latest-list">
-      {latestNews.map((latestItem, index) => (
-        <div
-          className="latest-item"
-          key={`${latestItem.id}-${index}`}
-          onClick={() => handleClick(latestItem.id)}
-        >
-          <img
-            src={latestItem.image || "/src/assets/images.png"}
-            alt={latestItem.title}
-            onError={(e) => (e.target.src = "/src/assets/images.png")}
-          />
-          <div className="latest-details">
-            <h2 className="latest-title">{latestItem.title}</h2>
-            <div className="line">
-              <p className="latest-date">{latestItem.date}</p>
-              <h4 className="latest-category">{latestItem.category.title}</h4>
-            </div>
-          </div>
-        </div>
-      ))}
+      <Grid2 container spacing={0}>
+        {latestNews.map((latestItem, index) => (
+          <Grid2 item xs={12} sm={6} md={4} key={`${latestItem.id}-${index}`}>
+            <Card
+              className="latest-item"
+              onClick={() => handleClick(latestItem.id)}
+              style={{ width: "100%" }}
+            >
+              <CardMedia
+                component="img"
+                src={latestItem.image || "/src/assets/images.png"}
+                alt={latestItem.title}
+                style={{ height: "100px" }}
+                onError={(e) => (e.target.src = "/src/assets/images.png")}
+              />
+              <CardContent className="latest-details">
+                <Typography
+                  variant="h6"
+                  sx={{
+                    fontSize: "1rem",
+                    fontWeight: "bold",
+                    display: "-webkit-box",
+                    overflow: "hidden",
+                    WebkitBoxOrient: "vertical",
+                    WebkitLineClamp: 3, // Limit to 3 lines
+                  }}
+                  className="latest-title"
+                >
+                  {latestItem.title}
+                </Typography>
+                <div className="line">
+                  <Typography className="latest-date">
+                    {new Date(latestItem.date).toLocaleDateString()}
+                  </Typography>
+                  <Typography variant="subtitle2" className="latest-category">
+                    {latestItem.category.title}
+                  </Typography>
+                </div>
+              </CardContent>
+            </Card>
+          </Grid2>
+        ))}
+      </Grid2>
 
       {hasMore && (
         <div className="load-more-wrapper">
-          <button className="load-more" type="button" onClick={handleLoadMore}>
+          <Button
+            className="load-more"
+            variant="contained"
+            onClick={handleLoadMore}
+            sx={{
+              margin: "20px auto",
+              padding: "10px 20px",
+              fontSize: "16px",
+              color: "#fff",
+              backgroundColor: "#00112f",
+              borderRadius: "5px",
+            }}
+          >
             Load More
-          </button>
+          </Button>
         </div>
       )}
     </div>
