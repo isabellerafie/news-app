035f1f7769759f24dc44b26c87d8b00f3b6d2c2c
Isabelle El Rafih <isorafi3@live.com>
2024-10-10 14:42:23 +0300

    hi

--- src/main.jsx
+++ src/main.jsx
@@ -7,3 +7,19 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.log("ServiceWorker registration failed: ", error);
+      });
+  });
+}
